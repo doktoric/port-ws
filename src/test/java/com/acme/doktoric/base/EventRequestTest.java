4e81592285a57818ae42b12a052464967a13794b
Richard Doktorics <doktoricsrichard@gmail.com>
2013-04-28 20:42:24 +0200

    added test to eventrequest

---
+++ src/test/java/com/acme/doktoric/base/EventRequestTest.java
@@ -0,0 +1,35 @@
+package com.acme.doktoric.base;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.acme.doktoric.types.base.EventRequest;
+import com.acme.doktoric.types.builders.RequestBuilder;
+import com.acme.doktoric.types.enums.Category;
+
+public class EventRequestTest {
+
+	private EventRequest eventRequest;
+	private String exceptedUrl="http://port.hu/pls/fe/festival.festival_list?i_city_id=-1&i_county_id=-1&i_cntry_id=44&i_topic_id=19&i_selected_date=19&i_view_date=2013-04-30-2013-04-01&i_selected_date=2013-04-01-2013-04-30";
+
+	@Before 
+	public void setUp() {
+		EventRequest eventRequest = RequestBuilder.create()
+				.withBaseUrl("http://port.hu/pls/")
+				.withCategory(Category.FESTIVAL)
+				.withFromDate("2013-04-01")
+				.withToDate("2013-04-30")
+				.build();
+	}
+	
+	@Test
+	public void TestingResponseUrlWithDefaultValues() throws IOException{
+		String returnUrl=eventRequest.getResponseUrl();
+		Assert.assertEquals(exceptedUrl, returnUrl);
+	}
+
+}
