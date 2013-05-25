f4f70aa27985df54067e68acde374a53beb97eb1
Richard Doktorics <doktoricsrichard@gmail.com>
2013-05-25 16:34:02 +0200

    new exception class for unsupported orders

---
+++ src/main/java/com/acme/doktoric/exceptions/UnsupportedRequestTypeException.java
@@ -0,0 +1,10 @@
+package com.acme.doktoric.exceptions;
+
+import com.acme.doktoric.types.enums.Category;
+
+public class UnsupportedRequestTypeException extends Exception {
+	
+	public UnsupportedRequestTypeException(Category category) {
+        super("Unsupported type in request :"+category);
+    }
+}
