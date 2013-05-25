f88be17f762c5827269a51b219c6949c26312d1c
Richard Doktorics <doktoricsrichard@gmail.com>
2013-05-25 16:33:28 +0200

    implemented new chain

--- src/main/java/com/acme/doktoric/tags/FestivalResponse.java
+++ src/main/java/com/acme/doktoric/tags/FestivalResponse.java
@@ -1,5 +1,7 @@
 package com.acme.doktoric.tags;
 
+import static com.acme.doktoric.types.base.Event.event;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -12,56 +14,29 @@ import com.acme.doktoric.types.base.Event;
 import com.acme.doktoric.types.builders.EventBuilder;
 import com.acme.doktoric.types.enums.Category;
 
-public class FestivalResponse implements PortResponse {
+public class FestivalResponse extends AbstractResponse {
 
-	private static final Logger logger = LoggerFactory.getLogger(PortResponse.class);
-
-	private Elements elements;
-	private List<Event> events;
+	private final Elements elements;
 
 
 	private FestivalResponse(Elements elements) {
 		this.elements = elements;
 		process();
 	}
-
-	public static final FestivalResponse festivalResponse(Elements elements) {
-		return new FestivalResponse(elements);
-	}
-
-	public Elements getElements() {
-		return elements;
-	}
-
-	public List<Event> getEvents() {
-		return events;
-	}
-
-	public void process() {
-		events = new ArrayList<Event>();
+	
+	public List<Event> process() {
+		List<Event> events = new ArrayList<Event>();
 		for (int i = 0; i < elements.size(); i++) {
 			String element = elements.get(i).text();
 			if (!element.trim().equals("H K Sze Cs P Szo V")) {
-				Event event = parse(element);
-				events.add(event);
+				events.add(parse(element));
 			}
-			
 		}
-
+		return events;
 	}
 
 	private Event parse(String event) {
-		Event returnEvent = null;
-		try {
-			returnEvent = specialEventParser(event).build();
-		} catch (Exception ex) {
-			logger.info("dsfsdfsdf");
-		}
-
-		return returnEvent;
-	}
-
-	private EventBuilder specialEventParser(String event) {
+		EventBuilder builder=EventBuilder.create();
 		try {
 			String parts[] = event.split(" - ");
 			String name = parts[0].trim();
@@ -82,44 +57,20 @@ public class FestivalResponse implements PortResponse {
 			}
 			fromDate = replaceMonthIntDateString(fromDate);
 
-			return EventBuilder.create().withEventCategory(Category.FESTIVAL)
+			builder.withEventCategory(Category.FESTIVAL)
 					.withEventName(name).withEventPlace(place)
 					.withFromDate(fromDate).withToDate(toDate);
 		} catch (Exception ex) {
 			logger.info("BAD__: " + event);
-			return null;
 		}
+		return event(builder);
 
 	}
 
-	private EventBuilder simpleEventParser(String event) throws Exception {
-		String parts[] = event.split("-");
-		String name = parts[0].trim();
-		String place = parts[1].trim().split(" ")[0].trim();
-		String fromDate = parts[1].trim().replace(place, "").trim();
-		String toDate = parts[2].trim();
-		fromDate = replaceMonthIntDateString(fromDate);
-		toDate = replaceMonthIntDateString(toDate);
-
-		return EventBuilder.create().withEventCategory(Category.FESTIVAL)
-				.withEventName(name).withEventPlace(place)
-				.withFromDate(fromDate).withToDate(toDate);
-	}
-
-	public static String replaceMonthIntDateString(String date) {
-		date = date.replace(".", "").replace(" ", "-").trim();
-		try {
-			for (int i = 0; i < MONTHS.length; i++) {
-				if (date.startsWith(MONTHS[i])) {
-					int year = Calendar.getInstance().get(Calendar.YEAR);
-					date = year + "-" + date;
-				}
-				date = date.replace(MONTHS[i], MONTHS_AS_NUMBER[i]);
-			}
-		} catch (Exception ex) {
-			logger.info(date);
-		}
-		return date;
+	
+	
+	public static final FestivalResponse festivalResponse(Elements elements) {
+		return new FestivalResponse(elements);
 	}
 
 }
