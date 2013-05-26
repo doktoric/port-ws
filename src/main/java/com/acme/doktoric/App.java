fa8bb680fe0fb241ba26161b603944fd42066ae2
Richard Doktorics <doktoricsrichard@gmail.com>
2013-05-26 19:46:39 +0200

    new test

--- src/main/java/com/acme/doktoric/App.java
+++ src/main/java/com/acme/doktoric/App.java
@@ -1,26 +1,22 @@
 package com.acme.doktoric;
 
-import static com.acme.doktoric.tags.FestivalResponse.festivalResponse;
+import static com.acme.doktoric.request.ExhibitionRequest.exhibitionRequest;
+import static com.acme.doktoric.request.FestivalRequest.festivalRequest;
 import static com.acme.doktoric.types.concrete.FromDate.fromDate;
 import static com.acme.doktoric.types.concrete.ToDate.toDate;
+
 import java.io.IOException;
 import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.acme.doktoric.exceptions.UnsupportedRequestTypeException;
-import com.acme.doktoric.tags.FestivalResponse;
-import com.acme.doktoric.tags.SimplePortRequest;
-import com.acme.doktoric.tags.PortResponse;
+import com.acme.doktoric.request.ExhibitionRequest;
+import com.acme.doktoric.request.FestivalRequest;
+import com.acme.doktoric.response.Response;
 import com.acme.doktoric.types.base.Event;
 import com.acme.doktoric.types.builders.RequestBuilder;
-import com.acme.doktoric.types.concrete.FromDate;
-import com.acme.doktoric.types.concrete.ToDate;
 import com.acme.doktoric.types.enums.Category;
 import com.acme.doktoric.types.enums.WebPages;
 
@@ -31,43 +27,49 @@ import com.acme.doktoric.types.enums.WebPages;
  */
 public class App {
 	
-	private static final Logger logger = LoggerFactory.getLogger(PortResponse.class);
+	private static final Logger logger = LoggerFactory.getLogger(Response.class);
 
 	
 	public static void main(String[] args) throws IOException, UnsupportedRequestTypeException {
 		
-		RequestBuilder builder=RequestBuilder.create()
+//		RequestBuilder builder=RequestBuilder.create()
+//				.withBaseUrl(WebPages.PORT)
+//				.withCategory(Category.FESTIVAL)
+//				.withFromDate(fromDate("2013-04-01"))
+//				.withToDate(toDate("2013-04-30"));
+//		List<Event> festivals=festivalRequest(builder).getResponse();
+		
+		RequestBuilder builder1=RequestBuilder.create()
 				.withBaseUrl(WebPages.PORT)
-				.withCategory(Category.FESTIVAL)
-				.withFromDate(fromDate("2013-04-01"))
-				.withToDate(toDate("2013-04-30"));
+				.withCategory(Category.EXHIBITION)
+				.withFromDate(fromDate("2013-05-20"))
+				.withToDate(toDate("2013-05-20"));
+		List<Event> exhibitions=exhibitionRequest(builder1).getResponse();
 		
-		List<Event> festivals=SimplePortRequest.simplePortRequest(builder).getResponse();
-		
-		for (Event festival : festivals) {
-			logger.info(festival.toString());
-		}
+//		for (Event festival : festivals) {
+//			logger.info(festival.toString());
+//		}
 	}	
 
-	private static void parseMainPage() throws IOException {
-		System.out.println("Kiemeltek");
-		Document doc = Jsoup.connect("http://port.hu/pls/fe/festival.index2")
-				.get();
-		Elements boxDiv = doc.select("#web_list_box_22>.background_color p b");
-		System.out.println("--------------------------------");
-		for (Element element : boxDiv) {
-			System.out.println(element.text());
-
-		}
-		System.out.println("################################");
-		System.out.println("Most Zajlok");
-		Elements nowComes = doc
-				.select(".box_border>table .white>table:nth-child(2)>tbody>tr:nth-child(4) a");
-		Elements dates = doc
-				.select(".box_border>table .white>table:nth-child(2)>tbody>tr:nth-child(4) span");
-		for (int i = 0; i < nowComes.size(); i++) {
-			System.out.println(nowComes.get(i).text() + "  "
-					+ dates.get(i).text());
-		}
-	}
+//	private static void parseMainPage() throws IOException {
+//		System.out.println("Kiemeltek");
+//		Document doc = Jsoup.connect("http://port.hu/pls/fe/festival.index2")
+//				.get();
+//		Elements boxDiv = doc.select("#web_list_box_22>.background_color p b");
+//		System.out.println("--------------------------------");
+//		for (Element element : boxDiv) {
+//			System.out.println(element.text());
+//
+//		}
+//		System.out.println("################################");
+//		System.out.println("Most Zajlok");
+//		Elements nowComes = doc
+//				.select(".box_border>table .white>table:nth-child(2)>tbody>tr:nth-child(4) a");
+//		Elements dates = doc
+//				.select(".box_border>table .white>table:nth-child(2)>tbody>tr:nth-child(4) span");
+//		for (int i = 0; i < nowComes.size(); i++) {
+//			System.out.println(nowComes.get(i).text() + "  "
+//					+ dates.get(i).text());
+//		}
+//	}
 }
