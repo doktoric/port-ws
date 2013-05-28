1778e5d9783cdb34ce249c6accd1788910b76b95
Richard Doktorics <doktoricsrichard@gmail.com>
2013-05-28 22:37:53 +0200

    child added

--- src/main/java/com/acme/doktoric/request/ChildRequest.java
+++ src/main/java/com/acme/doktoric/request/ChildRequest.java
@@ -1,5 +1,12 @@
 package com.acme.doktoric.request;
 
+import com.acme.doktoric.exceptions.UnsupportedRequestTypeException;
+import com.acme.doktoric.types.base.Event;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Ricsi
@@ -7,5 +14,14 @@ package com.acme.doktoric.request;
  * Time: 22:33
  * To change this template use File | Settings | File Templates.
  */
-public class ChildRequest {
+public class ChildRequest extends AbstractRequest {
+    @Override
+    public Elements getResponseBody() throws IOException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<Event> getResponse() throws IOException, UnsupportedRequestTypeException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
