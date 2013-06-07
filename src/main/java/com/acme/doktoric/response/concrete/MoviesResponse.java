d156bda11db6f17d193169f501b86c26cf9ab4d4
Richard Doktorics <doktoricsrichard@gmail.com>
2013-06-07 23:27:10 +0200

    response build

--- src/main/java/com/acme/doktoric/response/concrete/MoviesResponse.java
+++ src/main/java/com/acme/doktoric/response/concrete/MoviesResponse.java
@@ -2,9 +2,13 @@ package com.acme.doktoric.response.concrete;
 
 import com.acme.doktoric.response.AbstractResponse;
 import com.acme.doktoric.types.base.Event;
+import com.acme.doktoric.types.builders.EventBuilder;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.acme.doktoric.types.base.Event.event;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,14 +27,30 @@ public class MoviesResponse extends AbstractResponse {
 
     @Override
     public List<Event> process() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        List<Event> events = new ArrayList<Event>();
+        for (int i = 0; i < elements.size() - 1; i += 2) {
+            events.add(parse(elements.get(i).text(), elements.get(i + 1).text()));
+        }
+        return events;
     }
 
-    public static final  MoviesResponse moviesResponse(Elements elements) {
+    public static final MoviesResponse moviesResponse(Elements elements) {
         return new MoviesResponse(elements);
     }
 
-    protected Event parse(String event) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    protected Event parse(String eventPlace, String eventDescripton) {
+        eventPlace= eventPlace.replace(String.valueOf((char) 160), " ");
+        eventDescripton= eventDescripton.replace(String.valueOf((char) 160), " ");
+
+        EventBuilder builder=EventBuilder.create();
+        builder.withEventName(eventPlace);
+        String[] parts=eventDescripton.split(DAYS);
+        builder.withEventPlace(parts[0]);
+
+
+
+        logger.info(eventPlace);
+        logger.info(eventDescripton);
+        return event(builder);
     }
 }
