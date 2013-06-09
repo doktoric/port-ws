14ac4f7e762d5e7a34ac294d59cfb6ebef28c71a
Richard Doktorics <doktoricsrichard@gmail.com>
2013-06-09 19:06:22 +0200

    new class

---
+++ src/main/java/com/acme/doktoric/provider/RowProvider.java
@@ -0,0 +1,11 @@
+package com.acme.doktoric.provider;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ricsi
+ * Date: 2013.06.09.
+ * Time: 12:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class RowProvider {
+}
